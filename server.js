56293c8115bdbe121a609fbcf25c864d64fb7240
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-06-17 15:54:09 -0400

    able to post and get

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+// Add Access Control Allow Origin headers
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
+
+app.use(
+    cors({
+        origin: '*',
+    })
+)
 
 db.once('open', () => {
     app.listen(PORT, () => {
