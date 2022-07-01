578b9043216ebe7f12f3ab758dc76bc3bf2a0b9e
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-07-01 15:42:16 -0400

    added boiler plate bulma footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-import HeatMap from "../componets/heatmap1"
+
 import NavBar from "../componets/navBar"
+import Footer from "../componets/footer"
 export default function Home() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Home</h2>
         <NavBar/>
+
+        <Footer/>
 
         
 
