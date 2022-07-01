1a528ff2f1b631f75e51cd7df947635154fec066
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-07-01 07:37:56 -0400

    created context react APi

--- client/src/Context/GlobalState.js
+++ client/src/Context/GlobalState.js
@@ -3,5 +3,26 @@ import React, { createContext, useReducer } from 'react';
 // INital state
 
 const initialState = {
-    
+
+    userData: [
+        {username, x: [1,2,23,2,32,], y:[1,23,23,2,3], time: [23,123,12,3,42]}
+
+    ]
+
+}
+
+export const GlobalContext = createContext(initialState);
+
+// Provider componet 
+
+export const GlobalProvider = ({ children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+
+    return (<GlobalContext.Provider value={{
+        userData:state.userData
+    }}>
+    {children}
+    </GlobalContext.Provider>);
+
 }
